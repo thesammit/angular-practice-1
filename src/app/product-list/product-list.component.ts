84ed5e59189465d74a634145946b946e393279f3
thesammit <sammit90@gmail.com>
2018-08-13 11:36:56 +0530

    Introduction of the service for Products.

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -13,58 +14,7 @@ export class ProductListComponent implements OnInit {
   private _listFilter: string;
   private _listFilterFrom: string;
   private _listFilterTo: string;
-  products: IProduct[] =[
-    {
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2016",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "rating": 3.2,
-      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-    },
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2016",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "rating": 4.2,
-      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-    },
-    {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2016",
-      "description": "Curved claw steel hammer",
-      "price": 8.9,
-      "rating": 4.8,
-      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-    },
-    {
-      "productId": 8,
-      "productName": "Saw",
-      "productCode": "TBX-0022",
-      "releaseDate": "May 15, 2016",
-      "description": "15-inch steel blade hand saw",
-      "price": 11.55,
-      "rating": 3.7,
-      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
-    },
-    {
-      "productId": 10,
-      "productName": "Video Game Controller",
-      "productCode": "GMG-0042",
-      "releaseDate": "October 15, 2015",
-      "description": "Standard two-button video game controller",
-      "price": 35.95,
-      "rating": 4.6,
-      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
-    }
-  ];
+  products: IProduct[];
   filteredProducts: IProduct[];
   productHeaders: string[];
   headerMap: Object;
@@ -72,7 +22,7 @@ export class ProductListComponent implements OnInit {
   textWidth: number = 55;
   recievedMessage: string;
  
-  constructor() {
+  constructor(private productService: ProductService) {
     this.productHeaders = ['Product','Code','Availabe','Price','Rating'];
     this.filterType = this.productHeaders[0];
     this.headerMap = {
@@ -85,6 +35,7 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.products = this.productService.getProducts();
     this.filteredProducts = this.products;
   }
 
@@ -126,6 +77,7 @@ export class ProductListComponent implements OnInit {
       product[this.headerMap[this.filterType]].toLowerCase().indexOf(criteria) !== -1
     );
   }
+  
   filterProductsRange(criteriaFrom: string, criteriaTo: string): IProduct[] {
     this.recievedMessage = "";
     criteriaTo = +criteriaFrom > +criteriaTo ? null : criteriaTo;
